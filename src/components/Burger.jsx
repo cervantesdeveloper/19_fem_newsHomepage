10638d401da17f10eca6f03b9d8c672d594f2f1f
Victor <cervantesdeveloper@gmail.com>
2023-05-15 17:42:04 -0600

    I have changed the images route

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -4,8 +4,8 @@ import PropTypes from "prop-types"
 function Burger({myClick, clicked}){
     return(
         clicked 
-            ? <img src="src/assets/icon-menu-close.svg" className="btnClose" alt="close" onClick={myClick}/>
-            : <img src="src/assets/icon-menu.svg" alt="menu" onClick={myClick}/>
+            ? <img src="../src/assets/icon-menu-close.svg" className="btnClose" alt="close" onClick={myClick}/>
+            : <img src="../src/assets/icon-menu.svg" alt="menu" onClick={myClick}/>
             
     )
 }
--- src/components/RelatedNews.jsx
+++ src/components/RelatedNews.jsx
@@ -4,21 +4,21 @@ function RelatedNews(){
     return(
         <div className="related container">
             <Related 
-                image="src/assets/image-retro-pcs.jpg"
+                image="../src/assets/image-retro-pcs.jpg"
                 number="01"
                 heading="Reviving Retro PCs"
                 caption="What happens when old PCs are given modern upgrades?"
             />
 
             <Related 
-                image="src/assets/image-top-laptops.jpg"
+                image="../src/assets/image-top-laptops.jpg"
                 number="02"
                 heading="Top 10 Laptops of 2022"
                 caption="Our best picks for various needs and budgets."
             />
 
             <Related 
-                image="src/assets/image-gaming-growth.jpg"
+                image="../src/assets/image-gaming-growth.jpg"
                 number="03"
                 heading="The Growth of Gaming"
                 caption="How the pandemic has sparked fresh opportunities."
